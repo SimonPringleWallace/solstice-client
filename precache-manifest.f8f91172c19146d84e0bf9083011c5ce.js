03a922d23914b7f976b3c70efd1ce3484580814f
SimonPringleWallace <s.pringlewallace@gmail.com>
2018-12-09 21:41:43 -0500

    deploy task

---
+++ precache-manifest.f8f91172c19146d84e0bf9083011c5ce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3414f5fbd31b8987091512f313af68c0",
+    "url": "/solstice-client/static/media/solar-field.3414f5fb.jpg"
+  },
+  {
+    "revision": "bf1fc460541c936654f876a4e926a08f",
+    "url": "/solstice-client/static/media/commsolar.bf1fc460.gif"
+  },
+  {
+    "revision": "b091980a26eadd2bc8ba",
+    "url": "/solstice-client/static/js/runtime~main.b091980a.js"
+  },
+  {
+    "revision": "c06d501d9efcb31058a3",
+    "url": "/solstice-client/static/js/main.c06d501d.chunk.js"
+  },
+  {
+    "revision": "65819da584eeef4ed80c",
+    "url": "/solstice-client/static/js/1.65819da5.chunk.js"
+  },
+  {
+    "revision": "c06d501d9efcb31058a3",
+    "url": "/solstice-client/static/css/main.aad0a182.chunk.css"
+  },
+  {
+    "revision": "ff81ee60e609824813b65655ec787a8b",
+    "url": "/solstice-client/index.html"
+  }
+];
